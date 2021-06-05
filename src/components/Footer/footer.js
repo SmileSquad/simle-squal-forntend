fffbb19b842e2741b2b9ecdbca98c3f70828e046
Raghad <raghadmustafa96@yahoo.com>
2021-06-06 00:15:22 +0300

    add footer

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -1,9 +1,54 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import fb from '../../assets/fb.png'
+import tw from '../../assets/tw.png'
+import google from '../../assets/google.png'
+
+import './footer.scss';
 
 // Footer goes in all pages
 // see the wireframe
+
 function footer() {
-  return <div>Footer</div>;
+  return <div className="footer">
+    <div>
+      <ul className="footerUl">
+        <li>
+          <NavLink exact to="/">
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/profile">ABout</NavLink>
+        </li>
+      </ul>
+    </div>
+    <div>
+      <ul className="socialMedia">
+        <li>
+          <NavLink to="/">
+            <img src={fb} />
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/">
+            <img src={google} />
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/">
+            <img src={tw} />
+          </NavLink>
+        </li>
+      </ul>
+    </div>
+    <div className="discretion">
+      <p >
+        an exciting game to make people smile and making communication easier by play with each other and make new friend
+    </p>
+    </div>
+  </div>
 }
+
 
 export default footer;
