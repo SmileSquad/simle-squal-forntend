db4edb218bda53dfc3118be8f7370673d89b8a2c
fatensamman <arch.fatensamman@gmail.com>
2021-06-06 05:16:30 +0300

    vsleader

--- src/components/Leaderboard/leaderboard.js
+++ src/components/Leaderboard/leaderboard.js
@@ -1,10 +1,83 @@
 import React from 'react';
+import './leaderboard.scss';
+import Card from 'react-bootstrap/Card';
+import leaderimg from '../../assets/cardL.png'
 
 // Home Page
 // top players from the server handled by redux
 
 function leaderboard() {
-  return <div>leaderboard</div>;
+  return (
+    <div className="leaderboard">
+      <div className="title">
+        <h4>Leaderboard</h4>
+        <p> Be One of Our Top  Players</p>
+      </div>
+      <div className="cards">
+       
+        <Card className="bg-dark text-white">
+          <Card.Img src={leaderimg} alt="Card image" />
+          <Card.ImgOverlay>
+            <Card.Text className="num" >1</Card.Text>
+            <Card.Title className="wName">Winner Name</Card.Title>
+            <Card.Text className="wRatio">
+              Win Ratio 90%
+    </Card.Text>
+          </Card.ImgOverlay>
+        </Card>
+        <Card className="bg-dark text-white">
+          <Card.Img src={leaderimg} alt="Card image" />
+          <Card.ImgOverlay>
+            <Card.Text className="num" >2</Card.Text>
+            <Card.Title className="wName">Winner Name</Card.Title>
+            <Card.Text className="wRatio">
+              Win Ratio 90%
+    </Card.Text>
+          </Card.ImgOverlay>
+        </Card>
+        <Card className="bg-dark text-white">
+          <Card.Img src={leaderimg} alt="Card image" />
+          <Card.ImgOverlay>
+            <Card.Text className="num" >3</Card.Text>
+            <Card.Title className="wName">Winner Name</Card.Title>
+            <Card.Text className="wRatio">
+              Win Ratio 90%
+    </Card.Text>
+          </Card.ImgOverlay>
+        </Card>
+        <Card className="bg-dark text-white">
+          <Card.Img src={leaderimg} alt="Card image" />
+          <Card.ImgOverlay>
+            <Card.Text className="num" >4</Card.Text>
+            <Card.Title className="wName">Winner Name</Card.Title>
+            <Card.Text className="wRatio">
+              Win Ratio 90%
+    </Card.Text>
+          </Card.ImgOverlay>
+        </Card>
+        <Card className="bg-dark text-white">
+          <Card.Img src={leaderimg} alt="Card image" />
+          <Card.ImgOverlay>
+            <Card.Text className="num" >5</Card.Text>
+            <Card.Title className="wName">Winner Name</Card.Title>
+            <Card.Text className="wRatio">
+              Win Ratio 90%
+    </Card.Text>
+          </Card.ImgOverlay>
+        </Card>
+        <Card className="bg-dark text-white">
+          <Card.Img src={leaderimg} alt="Card image" />
+          <Card.ImgOverlay>
+            <Card.Text className="num" >6</Card.Text>
+            <Card.Title className="wName">Winner Name</Card.Title>
+            <Card.Text className="wRatio">
+              Win Ratio 90%
+    </Card.Text>
+          </Card.ImgOverlay>
+        </Card>
+      </div>
+    </div>
+  )
 }
 
 export default leaderboard;
