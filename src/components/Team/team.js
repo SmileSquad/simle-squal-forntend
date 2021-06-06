fdbe8f351e70960c64c6453378ab5f5639d1a19c
Raghad <raghadmustafa96@yahoo.com>
2021-06-06 07:20:19 +0300

    aboutUs

--- src/components/Team/team.js
+++ src/components/Team/team.js
@@ -1,23 +1,39 @@
 import React from 'react';
+import './team.scss';
+import raghad from '../../assets/raghad.png'
+import faten from '../../assets/faten.png'
+import omar from '../../assets/omar.png'
+import ayoub from '../../assets/ayoub.png'
+import bahaa from '../../assets/bahaa.png'
 
 // about us page
-
 // add the team images and links
 
 function team() {
   return (
-    <>
-  <div>team images</div>
-  <div>team images</div>
-  <div>team images</div>
-  <div>team images</div>
-  <div>team images</div>
-  <div>team images</div>
-  <div>team images</div>
- 
-  
+    <div className="aboutUs" >
 
-    </>
+      <div className="aboutUs2">
+        <div>
+          <img src={ayoub} />
+        </div>
+        <div>
+          <img src={omar} />
+        </div>
+        <div>
+          <img src={bahaa} />
+        </div>
+      </div>
+
+      <div className="aboutUs2">
+        <div>
+          <img src={faten} />
+        </div>
+        <div>
+          <img src={raghad} />
+        </div>
+      </div>
+    </div>
   )
 }
 
