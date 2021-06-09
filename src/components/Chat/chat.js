c961ece0ba39ad2baa8a6639d8ee772017d666b1
fatensamman <arch.fatensamman@gmail.com>
2021-06-10 00:57:23 +0300

    fix

--- src/components/Chat/chat.js
+++ src/components/Chat/chat.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './chat.scss'
-
+import chatimg from '../../assets/chat.png'
 // profile Page
 // should be accessable only if the user logged in
 // global chat with all players
@@ -8,6 +8,7 @@ import './chat.scss'
 function chat() {
   return <div>
    <h5> Global Chat</h5>
+   <img   src={chatimg} alt="chatimg"/>
     </div>;
 }
 
